65e5e569051f42888f86345d567ab92b096de4a5
Deepak k <deepakkannan107@gmail.com>
2024-11-22 10:52:49 +0530

    exercise

--- sttring-method/index.js
+++
@@ -1,5 +0,0 @@
-const email = "deepak@gmail.com";
-let username = email.slice(0 , email.indexOf ("@"));
-let extension  =  email.slice(email.indexOf ("@"));
-console.log (username);
-console.log (extension);
